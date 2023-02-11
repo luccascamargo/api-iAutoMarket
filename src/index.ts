76c710bbcc335a389b329967ec54f2e65979a091
Lucas Camargo <lucascamargo.dev@gmail.com>
2023-02-11 12:07:57 -0300

    fix: Ajustes na api

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import express, { Request, Response, NextFunction } from "express";
-import cors from "cors";
 
 import { router } from "./routes";
 
@@ -7,38 +6,13 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-const allowedOrigins = [
-  "capacitor://localhost",
-  "ionic://localhost",
-  "http://localhost",
-  "http://localhost:8080",
-  "http://localhost:8100",
-];
-
-// Reflect the origin if it's in the allowed list or not defined (cURL, Postman, etc.)
-const corsOptions = {
-  origin: ({ origin, callback }: any) => {
-    if (allowedOrigins.includes(origin) || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error("Origin not allowed by CORS"));
-    }
-  },
-  credentials: true,
-  optionsSuccessStatus: 200,
-  exposedHeaders: ["set-cookie"],
-};
-
-app.options("*", cors(corsOptions));
-
 app.use((req: Request, res: Response, next: NextFunction): void => {
-  if (req.originalUrl === "/webhook-stripe") {
+  if (req.originalUrl === "/stripe_webhooks") {
     next();
   } else {
     express.json()(req, res, next);
   }
 });
-app.use(cors());
 app.use(router);
 
 app.listen(Number(port), "0.0.0.0", () => {
